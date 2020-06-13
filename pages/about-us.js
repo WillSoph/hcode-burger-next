fa867cca4d81b760475a816bf58be1334cb14941
bispielberg <gabriel_bispo@me.com>
2020-06-13 00:55:31 -0300

    bispinho about us

--- pages/about-us.js
+++ pages/about-us.js
@@ -14,6 +14,8 @@ export default function About() {
             <HeaderAbout />
             <section>
                 <div className={styles['primeira-parte']}>
+
+               
                     <div className={styles.corte}>
                         <img src="assets/images/hamburguer-about-us.png" alt="hamburguer" />
                     </div>
@@ -23,7 +25,7 @@ export default function About() {
                         <h3> HCODE Burger's Chef</h3><br></br>
 
                         <p> Gabriel Bispo ou "Bispinho", autodidata, transformou a paixão pela cozinha em profissão. Mais do que alguém responsável pela elaboração do cardápio, preparação dos pratos e combinação dos sabores, o chef é a alma da Hcode Burguer. Há sempre um grande chef por trás de um grande restaurante.
-                   </p><br></br>
+                   </p><br/> 
 
                         <p>Fundada na crença de que todos merecem um lugar onde podem escapar das complicações do mundo atual, HCODE Burger abriu seu primeiro restaurante em 6 de junho de 1996 na icônica Avenida Paulista em São Paulo. Nós nos inspiramos nas cozinhas e cultura de todo o mundo, mantendo-se fiel aos clássicos.
                         </p>
@@ -33,7 +35,9 @@ export default function About() {
                     <div className={styles.chef1}>
                         <img src="assets/images/Chefs-phto-signature.png" alt="Chefe-Bispinho" />
                     </div>
-                </div>
+
+                    </div>
+                
 
 
                 <div className={styles.galeria}>
@@ -49,7 +53,7 @@ export default function About() {
                         <div className={styles.container}>
                             <img src="assets/images/gallery/zhanjiang-chen-Wg-1.png" alt="" />
                             <div className={styles.overlay}>
-                                <div className={styles.hover}>SÃO PAULO - SP</div>
+                                <div className={styles.hover}>SÃO PAULO   SP</div>
                             </div>
                         </div>
 
@@ -98,7 +102,7 @@ export default function About() {
                         <div className={styles.container}>
                             <img src="assets/images/gallery/priscilla-du-preez-W-1.png" alt="" />
                             <div className={styles.overlay}>
-                                <div className={styles.hover}>NEW YORK USA</div>
+                                <div className={styles.hover}>NEW YORK  USA</div>
                             </div>
                         </div>
 
