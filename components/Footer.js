0240e743dbe4a02cf6b89a712ae4a391a6d6bba2
bispielberg <gabriel_bispo@me.com>
2020-06-14 19:43:58 -0300

    Quem Somos Completo

--- components/Footer.js
+++ components/Footer.js
@@ -7,17 +7,16 @@ export default function Footer() {
                 <div className={styles['flex-footer']}>
 
                     <div className={styles['Logo-footer']}>
-                        <img src="assets/logo/hcode-burger-logo-white.svg" alt="Logo do Chefe" />
+                        <a href="/"><img src="assets/logo/hcode-burger-logo-white.svg" alt="Logo do Chefe" /></a>
 
                     </div>
 
                     <div className={styles.Endereco}>
-                        <h6>Endereço:</h6>
-                        <p>Rua Hcodelab n5, Bairro Javascript CCS - HTML5</p>
+                        <h6>Endereço</h6>
+                        <p>Rua Hcodelab n5, <br />Bairro Javascript CCS <br /> HTML5</p>
                     </div>
 
                     <div className={styles.contato}>
-
                         <h6>Fale Conosco:</h6>
                         <p>(055) 9985-6325</p>
 
@@ -25,12 +24,12 @@ export default function Footer() {
 
 
                     <div className={styles.redes}>
-                        <h6>CHEFHAT NA NET</h6>
+                        <h6>Redes Sociais</h6>
                         <div className={styles.net}>
-                            <img src="assets/images/003-whatsapp.svg" className={styles.whatsapp} alt="Whatsapp" />
-                            <img src="assets/images/001-facebook.svg" className={styles.facebook} alt="Facebook" />
-                            <img src="assets/images/011-instagram.svg" className={styles.Instagram} alt="Instagram" />
-                            <img src="assets/images/013-twitter-1.svg" className={styles.Twitter} alt="Twitter" />
+                        <a href="https://web.whatsapp.com/"><img src="assets/images/003-whatsapp.svg" className={styles.whatsapp} alt="Whatsapp" /></a>
+                        <a href="https://www.facebook.com/hcodebr/"><img src="assets/images/001-facebook.svg" className={styles.facebook} alt="Facebook" /></a>
+                        <a href="https://www.instagram.com/hcodebrasil/"><img src="assets/images/011-instagram.svg" className={styles.Instagram} alt="Instagram" /></a>
+                        <a href="https://twitter.com/hcodebr?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><img src="assets/images/013-twitter-1.svg" className={styles.Twitter} alt="Twitter" /></a>
                         </div>
                     </div>
 
